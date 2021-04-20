f60c8a77ed6f5163f6e191037dff59dc9c2e73aa
José Luis Vilches <jose.luis.comvilches@gmail.com>
2021-04-20 02:23:13 -0300

    Insertamos el socket en fornt y el io en back

--- server/server.js
+++ server/server.js
@@ -1,20 +1,37 @@
-const express = require('express');
+const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
-const path = require('path');
+const path = require('path')
 
-const app = express();
+const app = express()
+let server = http.createServer(app)
 
-const publicPath = path.resolve(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const publicPath = path.resolve(__dirname, '../public')
+const port = process.env.PORT || 3000
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath))
 
+let io = socketIO(server)
 
+io.on('connection', (client)=>{
 
-app.listen(port, (err) => {
+    console.log('Usuario conectado');
 
-    if (err) throw new Error(err);
+    client.on('disconnect', ()=>{
+        console.log('Usuario desconectado');
+    })
 
-    console.log(`Servidor corriendo en puerto ${ port }`);
+    client.on('enviarMensaje', (mensaje)=>{
+        console.log(mensaje);
+    })
 
-});
+})
+
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err)
+
+    console.log(`Servidor corriendo en puerto ${ port }`)
+
+})
